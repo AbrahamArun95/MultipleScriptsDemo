af9d5351a16dd3467ff410f1ac90dcbe9da3e9b3
unknown <unknown>
2020-12-23 07:47:21 +0100

    the new custom request added

---
+++ Action.c
@@ -0,0 +1,20 @@
+Action()
+{
+	//THis is second script
+
+
+	web_custom_request("web_custom_request",
+		"URL=https://www.sample.com",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+
+	
+	
+	
+	
+	return 0;
+}
