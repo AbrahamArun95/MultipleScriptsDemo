55cbc3c2e7859a0c28b8e56686f5361062d8858f
unknown <unknown>
2020-12-23 08:54:24 +0100

    new comment added

---
+++ Action.c
@@ -0,0 +1,19 @@
+Action()
+{
+
+	web_custom_request("web_custom_request",
+		"URL=https://www.sample.com",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+
+	//THis is second script
+	
+	//new comment added
+	
+	
+	return 0;
+}
